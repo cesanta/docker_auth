bb448d3692bbe779ac49731044cf563af6c25b00
rojer <rojer@rojer.me>
2015-06-05 10:54:39 +0100

    Use html/template instead of printf for auth page

--- auth_server/server/google_auth.go
+++ auth_server/server/google_auth.go
@@ -20,6 +20,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -132,6 +133,7 @@ type GoogleAuth struct {
 	config *GoogleAuthConfig
 	db     *leveldb.DB
 	client *http.Client
+	tmpl   *template.Template
 }
 
 func NewGoogleAuth(c *GoogleAuthConfig) (*GoogleAuth, error) {
@@ -144,6 +146,7 @@ func NewGoogleAuth(c *GoogleAuthConfig) (*GoogleAuth, error) {
 		config: c,
 		db:     db,
 		client: &http.Client{Timeout: 10 * time.Second},
+		tmpl:   template.Must(template.New("google_auth").Parse(string(MustAsset("data/google_auth.tmpl")))),
 	}, nil
 }
 
@@ -173,7 +176,7 @@ func (ga *GoogleAuth) doGoogleAuth(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (ga *GoogleAuth) doGoogleAuthPage(rw http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(rw, string(MustAsset("data/google_auth.tmpl")), ga.config.ClientId)
+	ga.tmpl.Execute(rw, struct{ ClientId string }{ClientId: ga.config.ClientId})
 }
 
 // https://developers.google.com/identity/protocols/OAuth2WebServer#handlingtheresponse

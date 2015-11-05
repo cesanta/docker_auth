5a91340d367e2562d1d7444cd4de683d18f02dd7
Konrad Kleine <konrad.wilhelm.kleine@gmail.com>
2015-11-05 10:46:07 +0100

    Add MongoDB backend for ACLs
    This adds the ability to store ACLs in a MongoDB database. There's also
    a documentation file in docs/ACL_Backend_MongoDB.md that explains the
    document layout inside MongoDB.
    
    See also #38 for the initial discussion on this topic.
    
    * `cache_duration` as well as `operation_timeout` now are of type
      time.Duration to allow more human readable configuration like
      `"1h2m"`.
    * There was an unreachable call to `w.Close()` which I've moved right
      behind the error checks after the resource was opened as `defer
      w.Close()`.
    * I've changed most of the wording from `ACLs` to `ACL` because there is
      I really only mean one list but multiple ACL entries.
    * Outsourced the cache reloading into it's own member function
      `MaybeUpdateACLCache`.
    * When the server starts it no longer prints something like
      `Config from config.yml (3 users, 0 ACL entries)` but `Config from
      config.yml (3 users, 0 ACL static entries)`. Notice the word
      **static** to denote the fact that there maybe an ACL backend for which
      no ACL has been fetched yet.
    * Made mgo.DialInfo and embedded struct in ACLMongoDialInfo
    
    Signed-off-by: Konrad Kleine <konrad.wilhelm.kleine@gmail.com>

---
+++ auth_server/authz/acl_mongo.go
@@ -0,0 +1,138 @@
+package authz
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/golang/glog"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// ACLMongoConfig stores how to connect to the MongoDB server and how long
+// an ACL remains valid until new ones will be fetched.
+type ACLMongoConfig struct {
+	DialInfo      ACLMongoDialConfig `yaml:"dial_info,omitempty"`
+	Collection    string             `yaml:"collection,omitempty"`
+	CacheDuration time.Duration      `yaml:"cache_duration,omitempty"`
+}
+
+// ACLMongoDialConfig stores how we connect to the MongoDB server
+type ACLMongoDialConfig struct {
+	mgo.DialInfo `yaml:",inline"`
+	PasswordFile string `yaml:"password_file,omitempty"`
+}
+
+// Validate ensures the most common fields inside the mgo.DialInfo portion of
+// an ACLMongoDialInfo are set correctly as well as other fields inside the
+// ACLMongoConfig itself.
+func (c *ACLMongoConfig) Validate() error {
+	if len(c.DialInfo.DialInfo.Addrs) == 0 {
+		return errors.New("At least one element in acl_mongo.dial_info.addrs is required")
+	}
+	if c.DialInfo.DialInfo.Timeout.String() == "" {
+		return errors.New("acl_mongo.dial_info.timeout is required (e.g. \"10s\" for 10 seconds)")
+	}
+	if c.DialInfo.DialInfo.Database == "" {
+		return errors.New("acl_mongo.dial_info.database is required")
+	}
+	if c.Collection == "" {
+		return errors.New("acl_mongo.collection is required")
+	}
+	if c.CacheDuration.String() == "" {
+		return errors.New("acl_mongo.cache_duration is required (e.g. \"1m\" for one minute)")
+	}
+	return nil
+}
+
+type aclMongoAuthorizer struct {
+	config           *ACLMongoConfig
+	cache            ACLCache
+	staticAuthorizer Authorizer
+}
+
+// ACLCache caches ACL and remembers the last time it was updated
+type ACLCache struct {
+	ACL        ACL
+	LastUpdate time.Time
+}
+
+// NewACLMongoAuthorizer creates a new ACL Mongo authorizer
+func NewACLMongoAuthorizer(config *ACLMongoConfig) (Authorizer, error) {
+	return &aclMongoAuthorizer{config: config}, nil
+}
+
+func (mongoAuthorizer *aclMongoAuthorizer) Authorize(ai *AuthRequestInfo) ([]string, error) {
+	if mongoAuthorizer.config == nil {
+		return nil, fmt.Errorf("MongoDB ACL config is not set")
+	}
+
+	if err := mongoAuthorizer.MaybeUpdateACLCache(); err != nil {
+		return nil, err
+	}
+
+	return mongoAuthorizer.staticAuthorizer.Authorize(ai)
+}
+
+func (mongoAuthorizer *aclMongoAuthorizer) Stop() {
+	// Nothing to do.
+}
+
+func (mongoAuthorizer *aclMongoAuthorizer) Name() string {
+	return "mongo ACL"
+}
+
+// MaybeUpdateACLCache checks if the ACL cache has expired and depending on the
+// the result it updates the cache with the ACL from the MongoDB server. The
+// ACL will be stored inside the static authorizer instance which we use
+// to minimize duplication of code and maximize reuse of existing code.
+func (mongoAuthorizer *aclMongoAuthorizer) MaybeUpdateACLCache() error {
+	c := mongoAuthorizer.config
+
+	duration := time.Now().Sub(mongoAuthorizer.cache.LastUpdate)
+	glog.V(2).Infof("Duration since last update of ACL %s. Cache expires afer %s.", duration.String(), c.CacheDuration.String())
+
+	// Test if cache has ever been filled or needs to be updated
+	if len(mongoAuthorizer.cache.ACL) == 0 || duration.Seconds() > c.CacheDuration.Seconds() {
+		// Read in the password (if any)
+		var finalPassword string
+		if c.DialInfo.DialInfo.Password != "" {
+			finalPassword = c.DialInfo.DialInfo.Password
+		}
+		if c.DialInfo.PasswordFile != "" {
+			passBuf, err := ioutil.ReadFile(c.DialInfo.PasswordFile)
+			if err != nil {
+				return fmt.Errorf("Failed to read password file \"%s\": %s", c.DialInfo.PasswordFile, err)
+			}
+			finalPassword = string(passBuf)
+		}
+		c.DialInfo.DialInfo.Password = finalPassword
+
+		glog.Infof("Updating ACL from MongoDB (operation timeout %s)", c.DialInfo.DialInfo.Timeout.String())
+		session, err := mgo.DialWithInfo(&c.DialInfo.DialInfo)
+		if err != nil {
+			return err
+		}
+		defer session.Close()
+
+		mongoAuthorizer.cache.ACL = ACL{} // Reset ACL
+		collection := session.DB(c.DialInfo.DialInfo.Database).C(c.Collection)
+		err = collection.Find(bson.M{}).All(&mongoAuthorizer.cache.ACL)
+		if err != nil {
+			return err
+		}
+		mongoAuthorizer.cache.LastUpdate = time.Now()
+
+		// Create a new static authorizer reusing the ACL we fetched from MongoDB
+		mongoAuthorizer.staticAuthorizer, err = NewACLAuthorizer(mongoAuthorizer.cache.ACL)
+		if err != nil {
+			return err
+		}
+	} else {
+		glog.V(2).Infof("Using cached ACL from MongoDB.")
+	}
+
+	return nil
+}
---
+++ docs/ACL_Backend_MongoDB.md
@@ -0,0 +1,68 @@
+# ACL backend in MongoDB
+
+Maybe you want to manage your ACL from an external application and therefore
+need them to be stored outside of your auth_server's configuration file.
+
+For this purpose, there's a [MongoDB](https://www.mongodb.org/) ACL backend
+which can query an ACL from a MongoDB database.
+
+A typical ACL entry from the static YAML configuration file looks something like
+this:
+
+```
+- match: {account: "/.+/", name: "${account}/*"}
+  actions: ["push", "pull"]
+  comment: "All logged in users can push all images that are in a namespace beginning with their name"
+```
+
+Notice the use of a regular expression (`/.+/`), a placeholder (`${account}`),
+and in particular the `actions` array.
+
+The ACL entry as is it is stored inside the static YAML file can be mapped to
+MongoDB quite easily. Below you can find a list of ACL entries that are ready to
+be imported into MongoDB. Those ACL entries reflect what's specified in the
+`example/reference.yml` file under the `acl` section (aka static ACL).
+
+**reference_acl.json**
+
+```json
+{"match" : {"account" : "admin"}, "actions" : ["*"], "comment" : "Admin has full access to everything."}
+{"match" : {"account" : "test", "name" : "test-*"}, "actions" : ["*"], "comment" : "User \"test\" has full access to test-* images but nothing else. (1)"}
+{"match" : {"account" : "test"}, "actions" : [], "comment" : "User \"test\" has full access to test-* images but nothing else. (2)"}
+{"match" : {"account" : "/.+/"}, "actions" : ["pull"], "comment" : "All logged in users can pull all images."}
+{"match" : {"account" : "/.+/", "name" : "${account}/*"}, "actions" : ["*"], "comment" : "All logged in users can push all images that are in a namespace beginning with their name"}
+{"match" : {"account" : "", "name" : "hello-world"}, "actions" : ["pull"], "comment" : "Anonymous users can pull \"hello-world\"."}
+```
+
+**Note** that each document entry must span exactly one line or otherwise the
+`mongoimport` tool (see below) will not accept it.
+
+## Import reference ACLs into MongoDB
+
+To import the above specified ACL entries from the reference file, simply
+execute the following commands.
+
+### Ensure MongoDB is running
+
+If you don't have a MongoDB server running, consider to start it within it's own
+docker container:
+
+`docker run --name mongo-acl -d mongo`
+
+### Get mongoimport tool
+
+On Ubuntu this is a matter of `sudo apt-get install mongodb-clients`.
+
+### Import ACLs
+
+```bash
+MONGO_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongo-acl)
+mongoimport --host $MONGO_IP --db docker_auth --collection acl < reference_acl.json
+```
+
+This should print a message like this if everything was successful:
+
+```
+connected to: 172.17.0.4
+Wed Nov  4 13:34:15.816 imported 6 objects
+```

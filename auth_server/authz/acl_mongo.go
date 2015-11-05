829d1fb693f3bfb801bd8539f627d089ca1a01df
Konrad Kleine <konrad.wilhelm.kleine@gmail.com>
2015-11-05 10:46:07 +0100

    Add MongoDB backend for ACLs
    This adds the ability to store ACLs in a MongoDB database. There's also
    a documentation file in docs/ACL_Backend_MongoDB.md that explains the
    document layout inside MongoDB.
    
    See also #38 for the initial discussion on this topic.
    
    * There was an unreachable call to `w.Close()` which I've moved right
      behind the error checks after the resource was opened as `defer
      w.Close()`.
    * When the server starts it no longer prints something like
      `Config from config.yml (3 users, 0 ACL entries)` but `Config from
      config.yml (3 users, 0 ACL static entries)`. Notice the word
      **static** to denote the fact that there maybe an ACL backend for which
      no ACL has been fetched yet.
    * Made mgo.DialInfo and embedded struct in ACLMongoDialInfo
    * ACL cache is secured with a sync.RWMutex and allows multiple
      non-blocking reads.
    * MongoDB connection is pooled
    * ACL are updated in a background go routine which is controlled by an
      update ticker (time.Ticker). On startup and on every tick, the ACL
      will be updated.
    
    Signed-off-by: Konrad Kleine <konrad.wilhelm.kleine@gmail.com>

---
+++ auth_server/authz/acl_mongo.go
@@ -0,0 +1,156 @@
+package authz
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/golang/glog"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// ACLMongoConfig stores how to connect to the MongoDB server and how long
+// an ACL remains valid until new ones will be fetched.
+type ACLMongoConfig struct {
+	DialInfo   ACLMongoDialConfig `yaml:"dial_info,omitempty"`
+	Collection string             `yaml:"collection,omitempty"`
+	CacheTTL   time.Duration      `yaml:"cache_ttl,omitempty"`
+}
+
+// ACLMongoDialConfig stores how we connect to the MongoDB server
+type ACLMongoDialConfig struct {
+	mgo.DialInfo `yaml:",inline"`
+	PasswordFile string `yaml:"password_file,omitempty"`
+}
+
+// Validate ensures the most common fields inside the mgo.DialInfo portion of
+// an ACLMongoDialInfo are set correctly as well as other fields inside the
+// ACLMongoConfig itself.
+func (c *ACLMongoConfig) Validate() error {
+	if len(c.DialInfo.DialInfo.Addrs) == 0 {
+		return errors.New("At least one element in acl_mongo.dial_info.addrs is required")
+	}
+	if c.DialInfo.DialInfo.Timeout == 0 {
+		c.DialInfo.DialInfo.Timeout = 10 * time.Second
+	}
+	if c.DialInfo.DialInfo.Database == "" {
+		return errors.New("acl_mongo.dial_info.database is required")
+	}
+	if c.Collection == "" {
+		return errors.New("acl_mongo.collection is required")
+	}
+	if c.CacheTTL < 0 {
+		return errors.New("acl_mongo.cache_ttl is required (e.g. \"1m\" for 1 minute)")
+	}
+	return nil
+}
+
+type aclMongoAuthorizer struct {
+	lastCacheUpdate  time.Time
+	lock             sync.RWMutex
+	config           ACLMongoConfig
+	staticAuthorizer Authorizer
+	session          *mgo.Session
+	updateTicker     *time.Ticker
+}
+
+// NewACLMongoAuthorizer creates a new ACL Mongo authorizer
+func NewACLMongoAuthorizer(c ACLMongoConfig) (Authorizer, error) {
+	// Attempt to create a MongoDB session which we can re-use when handling
+	// multiple auth requests.
+
+	// Read in the password (if any)
+	if c.DialInfo.PasswordFile != "" {
+		passBuf, err := ioutil.ReadFile(c.DialInfo.PasswordFile)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to read password file \"%s\": %s", c.DialInfo.PasswordFile, err)
+		}
+		c.DialInfo.DialInfo.Password = strings.TrimSpace(string(passBuf))
+	}
+
+	glog.Infof("Creating MongoDB session (operation timeout %s)", c.DialInfo.DialInfo.Timeout)
+	session, err := mgo.DialWithInfo(&c.DialInfo.DialInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	authorizer := &aclMongoAuthorizer{
+		config:       c,
+		session:      session,
+		updateTicker: time.NewTicker(c.CacheTTL),
+	}
+
+	go authorizer.continuouslyUpdateACLCache()
+
+	return authorizer, nil
+}
+
+func (ma *aclMongoAuthorizer) Authorize(ai *AuthRequestInfo) ([]string, error) {
+	ma.lock.RLock()
+	defer ma.lock.RUnlock()
+
+	// Test if authorizer has been initialized
+	if ma.staticAuthorizer == nil {
+		return nil, fmt.Errorf("MongoDB authorizer is not ready")
+	}
+
+	return ma.staticAuthorizer.Authorize(ai)
+}
+
+func (ma *aclMongoAuthorizer) Stop() {
+	// This causes the background go routine which updates the ACL to stop
+	ma.updateTicker.Stop()
+
+	// Close connection to MongoDB database (if any)
+	if ma.session != nil {
+		ma.session.Close()
+	}
+}
+
+func (ma *aclMongoAuthorizer) Name() string {
+	return "MongoDB ACL"
+}
+
+// continuouslyUpdateACLCache checks if the ACL cache has expired and depending
+// on the the result it updates the cache with the ACL from the MongoDB server.
+// The ACL will be stored inside the static authorizer instance which we use
+// to minimize duplication of code and maximize reuse of existing code.
+func (ma *aclMongoAuthorizer) continuouslyUpdateACLCache() {
+	var tick time.Time
+	for ; true; tick = <-ma.updateTicker.C {
+		aclAge := time.Now().Sub(ma.lastCacheUpdate)
+		glog.V(2).Infof("Updating ACL at %s (ACL age: %s. CacheTTL: %s)", tick, aclAge, ma.config.CacheTTL)
+
+		// Get ACL from MongoDB
+		var newCache ACL
+		sessionCopy := ma.session.Copy()
+		collection := sessionCopy.DB(ma.config.DialInfo.DialInfo.Database).C(ma.config.Collection)
+		err := collection.Find(bson.M{}).All(&newCache)
+		sessionCopy.Close()
+		if err != nil {
+			glog.Errorf("Failed to update ACL from MongoDB. ERROR: %s", err)
+			// Only return the error if there's no authorizer we can re-use
+			if ma.staticAuthorizer == nil {
+				glog.Infof("No stale ACL available to be re-used")
+				continue
+			}
+			glog.Infof("Using stale ACL (Age: %s, TTL: %s)", aclAge, ma.config.CacheTTL)
+			continue
+		}
+
+		// Time to acquire a write lock on the ACL cache and update it
+		ma.lock.Lock()
+		ma.lastCacheUpdate = time.Now()
+		ma.staticAuthorizer, err = NewACLAuthorizer(newCache)
+		ma.lock.Unlock()
+
+		if err != nil {
+			glog.Errorf("Failed to create new static authorizer: %s", err)
+			continue
+		}
+	}
+}

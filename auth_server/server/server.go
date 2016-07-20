87bb6d668b817b42623699dec3312dafc1a82799
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-07-20 14:00:48 +0200

    Fix typo in server.go

--- auth_server/server/server.go
+++ auth_server/server/server.go
@@ -190,7 +190,7 @@ func (as *AuthServer) Authenticate(ar *authRequest) (bool, error) {
 			if err == authn.NoMatch {
 				continue
 			} else if err == authn.WrongPass {
-				glog.Warningf("Failed authenticateion with %s: %s", err)
+				glog.Warningf("Failed authentication with %s: %s", err)
 				return false, nil
 			}
 			err = fmt.Errorf("authn #%d returned error: %s", i+1, err)

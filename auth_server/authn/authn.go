bc4c2fb8a2d267fef8a2a94e1fd40bf1f76d0e7b
Matti R <matti@mdranta.net>
2020-10-11 23:25:22 -0400

    update year

--- auth_server/authn/authn.go
+++ auth_server/authn/authn.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2016 Cesanta Software Ltd.
+   Copyright 2020 Cesanta Software Ltd.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
